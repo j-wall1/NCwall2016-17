e2d4d4761610d08de437da09a2d8317e1ea06a7b
j-wall1 <j-wall.1@onu.edu>
2017-01-24 20:53:36 -0500

    Add files via upload

---
+++ HW/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="html CascadeStyleSheet.css">
+        <title>Hello, World Example.</title>
+    </head>
+    
+    <body>
+        Hello, World!
+    </body>
+</html>
