1a1f30ef87a74ae408d5b7fbb631e1fb13c28fc1
j-wall1 <j-wall.1@onu.edu>
2017-02-10 01:26:24 -0500

    Add files via upload

---
+++ CSSfun/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="CSSsheet.css">
+        <title>CSS fun</title>
+    </head>
+    <body>
+        <p id="intro">New webpage!! Please Enjoy!</p>
+        
+        <p id="box">This box is </p>
+        
+        <div class="dropdown">
+            <img src="nbproject/Rouge.jpeg" alt="Rouge One" width="100" height="50">
+        <div class="dropdown-content">
+            <img src="nbproject/Rouge.jpeg" alt="Rouge One" width="300" height="200">
+        <div class="desc">Rouge One, A Star Wars Story</div>
+        </div>
+        </div>
+    </body>
+</html>
