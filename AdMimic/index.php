776bec60ae26234cf2a063522e0c9d3803ddff8f
j-wall1 <j-wall.1@onu.edu>
2017-02-10 01:26:07 -0500

    Add files via upload

---
+++ AdMimic/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="Adstyle.css">
+        <title></title>
+    </head>
+    <body>
+        <div id="Border">
+            <div id="text">
+            <div id="SplitHeart">
+                <img src="images/Capture.PNG" alt="Earring" width="260" height="200">
+            </div>
+                <div id="Title">Lovebirds</div>
+                <div id="paragraph">Odin Lonning of Juneau nests the 
+                    Tlingit Lovebids, Raven and Eagle,
+                    in these <div id="Red">Valentine's</div> ready design.
+                    Sterling Silver, shown full size.</div>
+                <div id="WholeHeart">
+                    <img src="images/Neck.PNG" alt="Neckalace" width="220" height="150">
+                </div>
+                <div id="item">#N22080E Earrings, Fishhook..................$84</div>
+                <div id="item2">#N32080N Necklet with 18" chain..........$88</div>
+                <div id="handling">$9 handling per order</div>
+                <div id="Shop">Shop online or request a catalog</div>
+                <div id="Morganimage">
+                    <img src="images/morgan.png" alt="morgan" width="190" height="25"
+                </div>
+                <div id="Contact">800-324-4934 davidmorgan.com</div>
+                <div id="address">11812 N Creek Pkwy N, Ste&bull;Bothell, WA 98011</div>
+            </div>
+        </div>
+            
+            
+        </div>
+    </body>
+</html>
